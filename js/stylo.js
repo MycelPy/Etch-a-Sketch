fcbdf4ba30ed11fde521101dfabd690e1bae1fa5
MycelPy <matus.michalcik@gmail.com>
2022-03-20 13:10:04 +0100

    Added choose color.

--- js/stylo.js
+++ js/stylo.js
@@ -5,6 +5,7 @@ container.innerHTML =
     `<div class="row">${'<div class="cell"></div>'.repeat(n)}</div>`
     .repeat(n)
 
+// GET COLOR
 function getColorCode() {
     var makeColorCode = '0123456789ABCDEF';
     var code = '#';
@@ -14,23 +15,42 @@ function getColorCode() {
     return code;
 }
 
+// LOADING ELEMENTS
 const cells = document.querySelectorAll('.cell')
 const clear = document.querySelector('.clear')
 const rainbow = document.querySelector('.rainbow')
+const choose = document.querySelector('.choose')
+const slider = document.querySelector('.slider')
+const sliderValue = document.querySelector('.sliderValue')
+sliderValue.innerHTML = `Squares per side: ${slider.value}`;
 
+// UPDATING VALUE OF SLIDER
+slider.oninput = function() {
+    sliderValue.innerHTML = `Squares per side: ${this.value}`;
+    n = Number(this.value);
+
+}
+
+// DEFAULT BLACK COLOR ON HOVER
 cells.forEach(cell => cell.addEventListener('mouseover', () => {
     cell.setAttribute('style', 'background-color: black;')
 }))
 
+// CLEAR
 clear.addEventListener('click', () => {
     cells.forEach(cell => cell.setAttribute('style', 'background-color: white;'))
-    cells.forEach(cell => cell.addEventListener('mouseover', () => {
-        cell.setAttribute('style', 'background-color: black;')
-    }))
 })
 
+// RAINBOW COLOR
 rainbow.addEventListener('click', () => {
     cells.forEach(cell => cell.addEventListener('mouseover', () => {
         cell.setAttribute('style', `background-color: ${getColorCode()}`)
+    }))
+})
+
+// CHOOSE COLOR
+choose.addEventListener('click', (e) => {
+    cells.forEach(cell => cell.addEventListener('mouseover', () => {
+        cell.setAttribute('style', `background-color: ${e.target.getAttribute('data-current-color')}`)
     }))
 })
